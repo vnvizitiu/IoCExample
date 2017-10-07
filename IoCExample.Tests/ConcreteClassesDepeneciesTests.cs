70590a557ff80c4c6a7c67969a009bf1dbc8ef22
Vlad Vizitiu <vnvizitiu@gmail.com>
2017-10-07 17:04:46 +0300

    Implemented multiple dependencies fulfilment in depth and breath;

--- IoCExample.Tests/ConcreteClassesDepeneciesTests.cs
+++ IoCExample.Tests/ConcreteClassesDepeneciesTests.cs
@@ -1,7 +1,7 @@
-﻿using NUnit.Framework;
-
-namespace IoCExample.Tests
+﻿namespace IoCExample.Tests
 {
+    using NUnit.Framework;
+
     [TestFixture]
     class ConcreteClassesDepeneciesTests
     {
@@ -12,11 +12,59 @@ namespace IoCExample.Tests
 
             SimpleClass simpleClass = sut.Create<SimpleClass>();
 
-            Assert.That(simpleClass, Is.Not.Null);
+            Assert.That(simpleClass, Is.Not.Null, "The instance shouldn't be null");
+        }
+
+        [Test]
+        public void WhenRequiringAClassWithDepencies_ShouldCascadeAndCreateClass()
+        {
+            IoCExampleImplementation sut = new IoCExampleImplementation();
+
+            SimpleClass2 simpleClass2 = sut.Create<SimpleClass2>();
+
+            Assert.That(simpleClass2, Is.Not.Null, "The instance shouldn't be null");
+            Assert.That(simpleClass2.Dependency, Is.Not.Null, "The dependency should be injected via constructor and shouldn't be null");
+        }
+
+        [Test]
+        public void WhenRequiringAClassWithMultipleAndNestedDepencies_ShouldCascadeAndCreateClass()
+        {
+            IoCExampleImplementation sut = new IoCExampleImplementation();
+
+            SimpleClass3 simpleClass3 = sut.Create<SimpleClass3>();
+
+            Assert.That(simpleClass3, Is.Not.Null, "The instance shouldn't be null");
+            Assert.That(simpleClass3.Dependency1, Is.Not.Null, "The dependency should be injected via constructor and shouldn't be null");
+            Assert.That(simpleClass3.Dependency2, Is.Not.Null, "The dependency should be injected via constructor and shouldn't be null");
+
+            Assert.That(simpleClass3.Dependency1.Dependency, Is.Not.Null, "The nested dependency should be fulfilled as well");
         }
     }
 
-    internal class SimpleClass
+    public class SimpleClass3
+    {
+        public SimpleClass2 Dependency1 { get; }
+
+        public SimpleClass Dependency2 { get; }
+
+        public SimpleClass3(SimpleClass2 simpleClass2, SimpleClass simpleClass)
+        {
+            this.Dependency1 = simpleClass2;
+            this.Dependency2 = simpleClass;
+        }
+    }
+
+    public class SimpleClass2
+    {
+        public SimpleClass2(SimpleClass dependency)
+        {
+            Dependency = dependency;
+        }
+
+        internal SimpleClass Dependency { get; }
+    }
+
+    public class SimpleClass
     {
     }
 }
--- IoCExample/IoCExampleImplementation.cs
+++ IoCExample/IoCExampleImplementation.cs
@@ -1,12 +1,39 @@
-﻿using System;
-
-namespace IoCExample
+﻿namespace IoCExample
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
     public class IoCExampleImplementation
     {
         public T Create<T>()
         {
-            T instance = Activator.CreateInstance<T>();
+            return (T)this.Create(typeof(T));
+        }
+
+        private object Create(Type type)
+        {
+            ConstructorInfo constructorInfo = type.GetConstructors().Single();
+
+            List<object> parameters = null;
+            ParameterInfo[] constructorParameters = constructorInfo.GetParameters();
+            foreach (ParameterInfo parameterInfo in constructorParameters)
+            {
+                parameters = parameters ?? new List<object>();
+                parameters.Add(this.Create(parameterInfo.ParameterType));
+            }
+
+            object instance;
+            if (parameters != null)
+            {
+                instance = constructorInfo.Invoke(parameters.ToArray());
+            }
+            else
+            {
+                instance = Activator.CreateInstance(type);
+            }
+
             return instance;
         }
     }
